61188614b754b51ca94abc341561548f6ec04dd3
duskat <duskat@ya.ru>
2015-08-10 22:25:19 -0400

    создана фикстура для test_add_contact

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,76 +1,25 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-import unittest
 from contact import Group
+import pytest
+from application_contact import Application2
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
 
-class test_add_contact(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
+@pytest.fixture
+def app(request):
+    fixture = Application2()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
     
-    def test_add_contact(self):
-        self.login(username="admin", password="secret")
-        self.creat_new_contact(Group(firstname="Petr", lastname="Petrov", nickname="Petya", title="tester", company="Yandex"))
-        self.logout()
+def test_add_contact(app):
+    app.login(username="admin", password="secret")
+    app.creat_new_contact(Group(firstname="Petr", lastname="Petrov", nickname="Petya", title="tester", company="Yandex"))
+    app.logout()
 
-    def test_add_empty_contact(self):
-        wd = self.wd
-        self.login(username="admin", password="secret")
-        self.creat_new_contact(Group(firstname="", lastname="", nickname="", title="", company=""))
-        self.logout()
+def test_add_empty_contact(app):
+    app.login(username="admin", password="secret")
+    app.creat_new_contact(Group(firstname="", lastname="", nickname="", title="", company=""))
+    app.logout()
 
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-
-    def creat_new_contact(self, contact):
-        wd = self.wd
-        # init new contact creation
-        wd.find_element_by_link_text("add new").click()
-        # fill new contact form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        # submit new contact creation
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == '__main__':
-    unittest.main()
+def tearDown(self):
+    self.app.destroy()
