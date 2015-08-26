cf91af2cac8285afcc3e27188d131875ccc05ea2
duskat <duskat@ya.ru>
2015-08-25 23:16:41 -0400

    Добавлена сортировка списков в методы добавления и модификации групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,14 +2,22 @@
 from model.group import Group
 
 
+
 def test_add_group(app):
     odl_groups = app.group.get_group_list()
-    app.group.create(Group(name="adjl", header="dhjh", footer="hhjh"))
+    group = Group(name="adjl", header="dhjh", footer="hhjh")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(odl_groups) + 1 == len(new_groups)
+    odl_groups.append(group)
+    assert sorted(odl_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
 
 def test_emp_add_group(app):
     odl_groups = app.group.get_group_list()
-    app.group.create(Group(name="", header="", footer=""))
+    group = Group(name="", header="", footer="")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(odl_groups) + 1 == len(new_groups)
+    odl_groups.append(group)
+    assert sorted(odl_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -6,15 +6,20 @@ def test_modify_group_name(app):
     odl_groups = app.group.get_group_list()
     if app.group.coutn() == 0:
         app.group.create(Group(name="test"))
-    app.group.modify_first_group(Group(name="New group"))
+    group = Group(name="New group")
+    group.id = odl_groups[0].id
+    app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(odl_groups) == len(new_groups)
+    odl_groups[0] = group
+    assert sorted(odl_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_modify_group_header(app):
+"""def test_modify_group_header(app):
     odl_groups = app.group.get_group_list()
     if app.group.coutn() == 0:
         app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(header="New header"))
     new_groups = app.group.get_group_list()
     assert len(odl_groups) == len(new_groups)
+"""
