87952edbbebaca9d3d875013e12ff1fdf78cc452
duskat <duskat@ya.ru>
2015-09-03 09:43:19 -0400

    Добавлена генерация случайных данных в тест добавления группы

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,9 +1,22 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import pytest
+import random
+import string
+
+def rendom_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
 
-def test_add_group(app):
+testdata = [Group(name="", header="", footer="")] + [
+        Group(name=rendom_string("name", 10), header=rendom_string("header", 20), footer=rendom_string("footer", 20))
+        for i in range(5)
+]
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, group):
     odl_groups = app.group.get_group_list()
     group = Group(name="adjl", header="dhjh", footer="hhjh")
     app.group.create(group)
@@ -13,11 +26,3 @@ def test_add_group(app):
     assert sorted(odl_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-"""def test_emp_add_group(app):
-    odl_groups = app.group.get_group_list()
-    group = Group(name="", header="", footer="")
-    app.group.create(group)
-    new_groups = app.group.get_group_list()
-    assert len(odl_groups) + 1 == len(new_groups)
-    odl_groups.append(group)
-    assert sorted(odl_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)"""
