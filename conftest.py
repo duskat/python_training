3dec16914578c21126d678156751829f8ee245d7
duskat <duskat@ya.ru>
2015-09-03 23:37:28 -0400

    Передача параметров в тесты из командной строки при запуске

--- conftest.py
+++ conftest.py
@@ -9,15 +9,17 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--base_url")
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
-            fixture = Application()
+            fixture = Application(browser=browser, base_url=base_url)
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
-
+    
 @pytest.fixture (scope = "session", autouse=True)
 def stop(request):
     def fin():
@@ -25,3 +27,7 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--base_url", action="store", default="http://localhost/addressbook/")
