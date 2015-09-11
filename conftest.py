052d9e18e7f32bbd33fbf0c6d5bdbbc3c03829d3
duskat <duskat@ya.ru>
2015-09-10 20:46:03 -0400

    Загрузка тестовых данных из файлов JSON  для теста добавления группы

--- conftest.py
+++ conftest.py
@@ -5,6 +5,7 @@ from fixture.application import Application
 import json
 import os.path
 import importlib
+import jsonpickle
 
 fixture = None
 target = None
@@ -49,4 +50,5 @@ def load_from_module(module):
     return importlib.import_module("data.%s" % module).testdata
 
 def load_from_json(file):
-    pass
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
+        return jsonpickle.decode(f.read())
