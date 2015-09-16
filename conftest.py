145eba39124f47eddd0ea5d1a07933e18a814795
duskat <duskat@ya.ru>
2015-09-15 22:40:32 -0400

    Отключаемые проверки пользовательского интерфейса для теста на удаление группы

--- conftest.py
+++ conftest.py
@@ -39,9 +39,7 @@ def stop(request):
     request.addfinalizer(fin)
     return fixture
 
-def pytest_addoption(parser):
-    parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--target", action="store", default="target.json")
+
 
 def pytest_generate_tests(metafunc):
     for fixture in metafunc.fixturenames:
@@ -61,9 +59,19 @@ def db(request):
     request.addfinalizer(fin)
     return dbfixtute
 
+@pytest.fixture
+def check_ui(request):
+    return request.config.getoption("--check_ui")
+
+
 def load_from_module(module):
     return importlib.import_module("data.%s" % module).testdata
 
 def load_from_json(file):
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
         return jsonpickle.decode(f.read())
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--target", action="store", default="target.json")
+    parser.addoption("--check_ui", action="store_true")
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -2,7 +2,7 @@ __author__ = 'Dzmitry'
 from model.group import Group
 import random
 
-def test_delet_some_group(app, db):
+def test_delet_some_group(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="test"))
     odl_groups = db.get_group_list()
@@ -12,3 +12,5 @@ def test_delet_some_group(app, db):
     assert len(odl_groups)  - 1 == len(new_groups)
     odl_groups.remove(group)
     assert odl_groups == new_groups
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
