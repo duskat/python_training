9d5d96b299134d9bbd797cc43b53f1d297d02faa
duskat <duskat@ya.ru>
2015-09-21 22:36:18 -0400

    Ошибка при включении опции check_ui в тесте на удаление контакта

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -3,13 +3,15 @@ from model.contact import Contact
 import random
 
 
-def test_delet_some_contact(app, db):
+def test_delet_some_contact(app, db, check_ui):
     if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(firstname="test"))
     old_contacts = db.get_contact_list()
     contact = random.choice(old_contacts)
     app.contact.delet_contact_by_id(contact.id)
-    new_contacts = app.contact.get_contact_list()
+    new_contacts = db.get_contact_list()
     assert len(old_contacts) - 1 == len(new_contacts)
     old_contacts.remove(contact)
     assert old_contacts == new_contacts
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
